d60a212b4ed131f76dd9f6c7d1857344828dc840
Jibran Ibrahim Syed <jibranisyed@outlook.com>
2015-12-11 23:25:49 -0600

    Preliminary script for scaling with two buttons

---
+++ Gyra/Assets/Scripts/ScalePlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScalePlayer : MonoBehaviour 
+{
+	public float scaleSpeed = 9.0f;
+
+	private Transform theTransform; // Caching for performance
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.theTransform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 newScale = theTransform.localScale;
+
+		if(TwoButtonDelegator.GetGrowButton())
+		{
+			newScale += Vector3.one * (this.scaleSpeed * Time.deltaTime);
+		}
+		else if(TwoButtonDelegator.GetShrinkButton())
+		{
+			newScale += Vector3.one * (-this.scaleSpeed * Time.deltaTime);
+		}
+
+		this.theTransform.localScale = newScale;
+	}
+}
