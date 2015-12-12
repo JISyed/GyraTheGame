819e3f2b6aec0f68f9baec9bc8dae3977252224c
Jibran Ibrahim Syed <jibranisyed@outlook.com>
2015-12-12 14:56:22 -0600

    Added script to move object forward with physics

---
+++ Gyra/Assets/Scripts/MoveFoward.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MoveFoward : MonoBehaviour 
+{
+	public float speed = 10.0f;
+
+	private Transform theTransform;
+	private Rigidbody theRigidBody;
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.theTransform = this.transform;
+		this.theRigidBody = this.GetComponent<Rigidbody>();
+
+		// Quick start
+		this.theRigidBody.AddForce(this.theTransform.forward * this.speed, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//this.theRigidBody.AddForce(this.theTransform.forward * (this.speed* Time.deltaTime), ForceMode.Force);
+	}
+}
