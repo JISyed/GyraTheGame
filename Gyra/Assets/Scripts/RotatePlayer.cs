0bff76c8bd4bf39f39ecad2f65f137512a416313
Jibran Ibrahim Syed <jibranisyed@outlook.com>
2015-12-11 21:47:33 -0600

    Initial project

---
+++ Gyra/Assets/Scripts/RotatePlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class RotatePlayer : MonoBehaviour 
+{
+	public float rotateSpeed = 9.0f;
+
+	private Transform theTransform;	// Caching for performance
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.theTransform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 newRot = this.theTransform.eulerAngles;
+		newRot.Set(0.0f, newRot.y + this.rotateSpeed * Time.deltaTime, 0.0f);
+		this.theTransform.eulerAngles = newRot;
+	}
+}
