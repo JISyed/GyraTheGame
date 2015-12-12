a62dc006c484db0054bb3e450804b1a47a480b7a
Jibran Ibrahim Syed <jibranisyed@outlook.com>
2015-12-12 12:19:41 -0600

    Made singleton that controls scaling. Also made a helper function to convert from one scale range to another. Useful for game mechanics and effects

---
+++ Gyra/Assets/Scripts/ControlScale.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+public class ControlScale : MonoBehaviour 
+{
+	//
+	// Data
+	//
+
+	public float scaleSpeed = 45.0f;
+	public float startingScale = 12.0f;
+	public const float MinScale = 1.0f;
+	public const float MaxScale = 100.0f;
+
+	private float currentScaleValue;
+
+	private static ControlScale singetonInstance;
+
+
+	//
+	// Methods
+	//
+
+	void Awake()
+	{
+		ControlScale.singetonInstance = this;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.currentScaleValue = this.startingScale;
+	}
+
+	void OnDestroy()
+	{
+		ControlScale.singetonInstance = null;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(TwoButtonDelegator.GetGrowButton())
+		{
+			this.currentScaleValue += (this.scaleSpeed * Time.deltaTime);
+			if(this.currentScaleValue > ControlScale.MaxScale)
+			{
+				this.currentScaleValue = ControlScale.MaxScale;
+			}
+			
+		}
+		else if(TwoButtonDelegator.GetShrinkButton())
+		{
+			this.currentScaleValue += (-this.scaleSpeed * Time.deltaTime);
+			if(this.currentScaleValue < ControlScale.MinScale)
+			{
+				this.currentScaleValue = ControlScale.MinScale;
+			}
+		}
+	}
+
+
+
+	//
+	// Static Utilities
+	//
+
+	/// <summary>
+	/// 	Take a "from" number between 1 and 100 and convert it to a custom "to" range
+	/// </summary>
+	/// <param name="fromValue">Value must be between 1.0f and 100.0f</param>
+	/// <param name="toMin">Lowest value to convert to</param>
+	/// <param name="toMax">Highest value to convert to</param>
+	public static float Rescale(float fromValue, float toMin, float toMax)
+	{
+		float fromRange = ControlScale.MaxScale - ControlScale.MinScale;
+		float toRange = toMax - toMin;
+		float convertRatio = toRange / fromRange;
+		fromValue -= ControlScale.MinScale;
+		float toValue = convertRatio * fromValue;
+		return toValue + toMin;
+	}
+
+
+
+	//
+	// Properties
+	//
+
+	/// <summary>
+	/// 	Should return a float between 1.0f and 100.0f
+	/// </summary>
+	public static float CurrentScaleValue
+	{
+		get
+		{
+			Debug.Assert(ControlScale.singetonInstance != null, "ControlScale singeton needs to exist before using! Make one in the scene.");
+			return ControlScale.singetonInstance.currentScaleValue;
+		}
+	}
+}
--- Gyra/Assets/Scripts/ScalePlayer.cs
+++ Gyra/Assets/Scripts/ScalePlayer.cs
@@ -3,18 +3,18 @@ using System.Collections;
 
 public class ScalePlayer : MonoBehaviour 
 {
-	public float scaleSpeed = 9.0f;
+	//public float scaleSpeed = 9.0f;
 	public float minScale = 2.0f;
 	public float maxScale = 25.0f;
 
 	private Transform theTransform; // Caching for performance
-	private float currentScale;	// Uniform scaling
+	//private float currentScale;	// Uniform scaling
 
 	// Use this for initialization
 	void Start () 
 	{
 		this.theTransform = this.transform;
-		this.currentScale = this.theTransform.localScale.x;
+		//this.currentScale = this.theTransform.localScale.x;
 	}
 	
 	// Update is called once per frame
@@ -22,14 +22,14 @@ public class ScalePlayer : MonoBehaviour
 	{
 		Vector3 newScale = theTransform.localScale;
 
-		if(TwoButtonDelegator.GetGrowButton())
+		/*if(TwoButtonDelegator.GetGrowButton())
 		{
 			this.currentScale += (this.scaleSpeed * Time.deltaTime);
 			if(this.currentScale > this.maxScale)
 			{
 				this.currentScale = this.maxScale;
 			}
-			newScale.Set(this.currentScale, this.currentScale, this.currentScale);
+
 		}
 		else if(TwoButtonDelegator.GetShrinkButton())
 		{
@@ -38,9 +38,12 @@ public class ScalePlayer : MonoBehaviour
 			{
 				this.currentScale = this.minScale;
 			}
-			newScale.Set(this.currentScale, this.currentScale, this.currentScale);
-		}
+		}*/
 
+		float rawScale = ControlScale.CurrentScaleValue;
+		float finalScale = ControlScale.Rescale(rawScale, this.minScale, this.maxScale);
+
+		newScale.Set(finalScale, finalScale, finalScale);
 		this.theTransform.localScale = newScale;
 	}
 }
