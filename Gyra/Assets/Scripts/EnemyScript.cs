8bccabcaf14ffe4c8d578519ad6d503b5ea5630b
Jibran Ibrahim Syed <jibranisyed@outlook.com>
2015-12-12 22:26:47 -0600

    Enemies collide with core properly

---
+++ Gyra/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EnemyScript : MonoBehaviour 
+{
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag.Equals("Core"))
+		{
+			//Debug.Log("Miku!");
+			ControlScale.ShrinkForTime();
+			Destroy(this.gameObject);
+		}
+		else if(other.gameObject.tag.Equals("Arm"))
+		{
+			//Debug.Log("Crash!");
+		}
+	}
+}
