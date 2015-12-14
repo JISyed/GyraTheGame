b0b228f481415b7ac699153872ddfa4db0fdd43d
Jibran Ibrahim Syed <jibranisyed@outlook.com>
2015-12-13 19:47:54 -0600

    Help splash toggle works

---
+++ Gyra/Assets/Scripts/MenuController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UI = UnityEngine.UI;
+
+public class MenuController : MonoBehaviour 
+{
+	public UI.Text highscoreDisplay;
+	public GameObject helpPanel;
+	//public GameObject creditsPanel;
+
+	private int highscore = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Assert(this.highscoreDisplay != null, "Menu Controller needs a UI.Text to display the highscore!");
+		Debug.Assert(this.helpPanel != null, "Menu Controller needs the gameobject for the Help Panel!");
+		this.helpPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+
+	private void LoadHighscore()
+	{
+		if(PlayerPrefs.HasKey(ScoreController.HighScoreKey))
+		{
+			this.highscore = PlayerPrefs.GetInt(ScoreController.HighScoreKey);
+		}
+		else
+		{
+			this.highscore = 0;
+		}
+	}
+
+
+	public void ActivateHelpPanel()
+	{
+		this.helpPanel.SetActive(true);
+	}
+
+	public void DeactivateHelpPanel()
+	{
+		this.helpPanel.SetActive(false);
+	}
+
+}
---
+++ Gyra/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ScoreController : MonoBehaviour 
+{
+	public const string HighScoreKey = "highscore";
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
