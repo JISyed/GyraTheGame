a05b8f5dfccfa2c973beb81f1eb2e9926678f20a
Jibran Ibrahim Syed <jibranisyed@outlook.com>
2015-12-14 19:51:17 -0600

    Integrated music. Might be finished with the game.
    Music copyright information (Creative Commons):
    
    Start To Grow (cdk Mix) by cdk (c) 2013
    Ft: Jeris
    Licensed under a Creative Commons Attribution (3.0) license.
    http://ccmixter.org/files/cdk/43815
    http://creativecommons.org/licenses/by/3.0/

---
+++ Gyra/Assets/Scripts/PlayMusicInAnyScene.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayMusicInAnyScene : MonoBehaviour 
+{
+	private AudioSource theAudioSource;
+
+	// Null in beginning of program
+	private static PlayMusicInAnyScene onlyInstance = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Delete any new instnaces of the music player
+		if(PlayMusicInAnyScene.onlyInstance != null)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+
+		PlayMusicInAnyScene.onlyInstance = this;
+
+		this.theAudioSource = this.gameObject.GetComponent<AudioSource>();
+		this.theAudioSource.playOnAwake = false;
+		this.theAudioSource.loop = true;
+		this.theAudioSource.Play();
+
+		// This object will exist until the game closes
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
